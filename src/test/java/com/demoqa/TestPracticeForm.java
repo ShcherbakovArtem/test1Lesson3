ce40d9ed61a8128051f8dce09ffad6b19f6ed92b
artemshcherbakov <artshcherbakov99@gmail.com>
2022-10-27 13:04:24 +0300

    Tets practic form some troble

--- src/test/java/com/demoqa/TestPracticeForm.java
+++ src/test/java/com/demoqa/TestPracticeForm.java
@@ -4,6 +4,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -24,6 +27,7 @@ public class TestPracticeForm {
         $("#userEmail").setValue("artshcherbakov99@gmail.com");
         $("[for=gender-radio-1]").click();
         $("#userNumber").setValue("+79103445241");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/TNhs2h00Rc0.jpeg"));
         // $("#dateOfBirthInput").click();
        /* $(".react-datepicker__month-select").setValue("4");
         $(".react-datepicker__year-select").setValue("1999");
@@ -33,6 +37,11 @@ public class TestPracticeForm {
         $("[for=hobbies-checkbox-3]").click();
         //$("input#uploadPicture").uploadFromClasspath("TNhs2h00Rc0.jpeg");
         $("#currentAddress").setValue("la la la");
+        $(byText("Select State")).click();
+        $(byText("NCR")).click();
+        $(byText("Select City")).click();
+        $(byText("Gurgaon")).click();
+
         /*$("#state").setValue("Russia");
         $("#city").setValue("Voronez");*/
 
